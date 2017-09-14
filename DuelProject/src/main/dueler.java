2b0312903e6ca3acfba535c0a7c1c0ea6ed77120
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-09-14 10:42:49 -0400

    dueler

---
+++ DuelProject/src/main/dueler.java
@@ -0,0 +1,13 @@
+package main;
+
+public interface dueler {
+	
+	//print a taunt
+	//later: the taunt must be different each time the method is called
+	
+	void taunt();
+	
+	String getName();
+	
+	
+}
