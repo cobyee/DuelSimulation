d3953c7deebf0521108a87c4c1989cb495fd6114
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-09-14 11:44:44 -0400

    Updated Character A

--- DuelProject/src/main/CharacterA.java
+++ DuelProject/src/main/CharacterA.java
@@ -1,5 +1,22 @@
 package main;
 
-public class CharacterA {
-
+public class CharacterA implements Dueler {
+	
+	private int hp;
+	
+	public void taunt() {
+		System.out.println("You're dead meat.");
+	}
+	
+	public String getName() {
+		return "Coby Yee";
+	}
+	
+	public void setStartingHp(int hp){
+		this.hp = hp;
+	}
+	
+	public int getHp() {
+		return hp;
+	}
 }
